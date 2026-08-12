ff01e42bfe04cae581d51c42b3818bb9b17e7506
Luiz F <luiz.f.1@proton.me>
2026-08-12 13:09:33 -0300

    fix:login/register/logout

--- src/components/core/buttons/SignOutButton.tsx
+++ src/components/core/buttons/SignOutButton.tsx
@@ -1,10 +1,13 @@
-import { Box } from "@/components/ui/box"
-import { signOutFunction } from "@/services/auth/authFunctions"
-import { Button, ButtonText } from "@/components/ui/button"
+import { Box } from "@/components/ui/box";
+import { Button, ButtonText } from "@/components/ui/button";
+import { useSession } from "@/services/auth/session";
+
 export default function SignOutButton() {
+    const { signOut } = useSession();
+
     return (
         <Box>
-            <Button variant="destructive" size="sm" onPress={signOutFunction}>
+            <Button variant="destructive" size="sm" onPress={signOut}>
                 <ButtonText>SignOut</ButtonText>
             </Button>
         </Box>
